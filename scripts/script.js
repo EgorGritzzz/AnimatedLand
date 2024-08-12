3ae19f11005c4e6a1cb258086f564bb915b13ce9
EgorGritzzz <134557979+EgorGritzzz@users.noreply.github.com>
2024-08-12 23:09:06 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+window.onload = function (){
+
+   const preloader = document.querySelector('.preloader');
+    setTimeout(()=>{
+        preloader.style.display = 'none';
+       
+    }, 1500)
+
+   
+}
+
+
+
+
