64539f5485c1dafe5540a14c6e82d00436f99378
EgorGritzzz <134557979+EgorGritzzz@users.noreply.github.com>
2024-08-12 23:26:18 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,12 @@
 window.onload = function (){
-
    const preloader = document.querySelector('.preloader');
+   const header = document.querySelector('#header');
     setTimeout(()=>{
         preloader.style.display = 'none';
-       
+        header.scrollIntoView({ behavior: 'smooth' });
     }, 1500)
-
-   
 }
+
 
 
 
